039bfbb08b1d812c46b47d43acf3337f62fc1d48
Daiana <arenadaianam@gmail.com>
2023-09-13 21:21:41 -0300

    tp4

---
+++ src/Exercises/TP4/App.java
@@ -0,0 +1,45 @@
+package Exercises.TP4;
+
+import Exercises.TP4.MergeSort.MergeSort;
+import Exercises.TP4.QuickSort.QuickSort;
+
+public class App {
+
+    
+        // Datos de identificación    
+        //nombreApellido = "Daiana Arena";
+        //dni = "38629115";
+        // legajo = "VINF014686";
+
+        /* Una función que sirve para mostrar un array de tamaño n */
+        static void MostrarArray(int arr[])
+        {
+            int n = arr.length;
+            for (int i = 0; i < n; ++i)
+            
+            System.out.print(arr[i] + " ");
+        }
+        // Main
+        public static void main(String args[])
+        {
+
+            int arr[] = { 12, 11, 13, 5, 6, 7 };
+            System.out.println("\nArray original antes de MergeSort");
+            MostrarArray(arr);
+            MergeSort obMS = new MergeSort();
+            obMS.ordenar(arr, 0, arr.length - 1);
+            System.out.println("\nArray ordenado por Merge Sort");
+            MostrarArray(arr);
+            int arr2[] = { 12, 11, 13, 5, 6, 7 };
+            System.out.println("\nArray original antes de QuickSort");
+            MostrarArray(arr2);
+            int n = arr2.length;
+            QuickSort obQS = new QuickSort();
+            obQS.ordenar(arr2, 0, n-1);
+            System.out.println("\nArray ordenado por Quick Sort");
+            MostrarArray(arr2);
+        }
+
+    
+    
+}
---
+++ src/Exercises/TP4/QuickSort/QuickSort.java
@@ -0,0 +1,51 @@
+package Exercises.TP4.QuickSort;
+
+public class QuickSort {
+    /* Esta función toma el último elemento como pivote,
+    * coloca el elemento pivote en su posición correcta en la matriz
+    ordenada
+    * y coloca todos los elementos más pequeños (más pequeños que pivote)
+    * a la izquierda del pivote y todos los elementos mayores a la derecha
+    del pivote.
+    */
+    int particion(int arr[], int low, int high){
+        int pivot = arr[high];
+        int i = (low-1); // índice del elemento mas chico
+        for (int j=low; j<high; j++)
+        {
+        // Si el elemento actual es mas chico o igual que el pivot
+        if (arr[j] <= pivot)
+        {
+        i++;
+        // swap arr[i] and arr[j]
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        }
+        }
+        // Intercambia arr[i+1] y arr[high] (o pivot)
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+        }
+        /* Método principal que implementa quicksort
+        arr[] --> Array a ser ordenado,
+        low --> Comienzo de indice,
+        high --> Fin de indice */
+        public void ordenar(int arr[], int low, int high){
+            if (low < high) {
+                // Encuentra el índice de partición
+                int partitionIndex = particion(arr, low, high);
+        
+                // Ordena la parte izquierda del arreglo (elementos menores que el pivote)
+                ordenar(arr, low, partitionIndex - 1);
+        
+                // Ordena la parte derecha del arreglo (elementos mayores que el pivote)
+                ordenar(arr, partitionIndex + 1, high);
+            }
+        }
+    
+    
+
+}
